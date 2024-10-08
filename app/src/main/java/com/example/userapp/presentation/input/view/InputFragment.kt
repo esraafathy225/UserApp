cd46a74ab036c324686492e949eb170768bd0cec
esraa.fathy225 <esraa.fathy225@gmail.com>
2024-10-08 21:08:55 +0300

    Add input screen UI and navigation component

--- app/src/main/java/com/example/userapp/presentation/input/view/InputFragment.kt
+++ app/src/main/java/com/example/userapp/presentation/input/view/InputFragment.kt
@@ -1,4 +1,25 @@
 package com.example.userapp.presentation.input.view
 
-class InputFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.userapp.databinding.FragmentInputBinding
+
+class InputFragment : Fragment() {
+
+    private var _binding: FragmentInputBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentInputBinding.inflate(inflater,container,false)
+        val view  = binding.root
+
+        return view
+    }
 }
